58cc029e86ee77767022c717ca1e8cd3300079bc
Khaled123x <143716155+Khaled123x@users.noreply.github.com>
2023-09-01 15:27:46 +0600

    config.py

--- bot/config.py
+++ bot/config.py
@@ -19,8 +19,8 @@ try:
     APP_ID = config("20110964", cast=int)
     API_HASH = config("5f4a53387dff48bf9c4b4bab97491ad7")
     BOT_TOKEN = config("6403982406:AAEZdi6ZrOXXTmAypmAUu366SD-GGLMHjYM")
-    DEV = 1761465389
-    OWNER = config("HASAN")
+    DEV = 1287276743
+    OWNER = config("OWNER")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By KAMRUL (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMB = config("THUMBNAIL")
 except Exception as e:
